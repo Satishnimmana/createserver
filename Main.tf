dd36d2e912ff236bed4beb745764587b844cbcd0
Satishnimmana <satishnimmana31@gamil.com>
2022-09-04 16:28:39 +0530

    #7

--- Main.tf
+++ Main.tf
@@ -1,12 +1,21 @@
 
 
-backend "remote" {
- organization = "Smarttechcloud"
- 
-  workspaces {
-    name = "smartaction"
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
-}
+backend "remote" {
+  organization = "Smarttechcloud"
+
+   workspaces {
+     name = "smartaction"
+    }
+   }
+ }
 provider "aws"{
- region = ${{ secrets.AWS_REGION }}
+  region = ${{ secrets.AWS_REGION }}
 }
