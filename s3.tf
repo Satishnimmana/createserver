7fb4fd7fc07dcdc0280ae95d41330be55943d71f
Satishnimmana <satishnimmana31@gamil.com>
2022-09-04 17:11:42 +0530

    s3 resource cahnges

--- s3.tf
+++ s3.tf
@@ -1,4 +1,9 @@
-resource "aws_s3_bucket_acl" "techcloud" {
-  bucket = "techcloud"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
   acl    = "private"
+  
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
